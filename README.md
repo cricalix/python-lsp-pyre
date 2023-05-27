feb6cc0bb2a20a2a50952626d785511df1395b0a
Duncan <github.commits@cricalix.net>
2023-05-27 09:46:27 +0100

    Enable strict mode, update docs, use internal Settings object
    Updates the default pyre configuration to be strict,
    and updates the documentation to match. Introduces an
    internal settigs object, because I'm not a fan of
    dicts and the typing thereof (yes, TypedDict exists).
    
    * Introduces the Settings dataclass
    * Uses it to convert the pylsp settings dict to a typed object
    * Defaults pyre to strict mode when writing out the initial config file
    * Updates the documentation to reflect this strict mode
    * Updates the documentation to explain squelching of Pyre lints
    * Adds source commentary about pyre's output vs LSP w.r.t. lints per file
    * Bumps the version

--- README.md
+++ README.md
@@ -4,6 +4,14 @@ This is a plugin for the [Python LSP Server](https://github.com/python-lsp/pytho
 
 It implements support for calling Meta's [Pyre type checker](https://github.com/facebook/pyre-check) via a subprocess.
 
+Pyre does offer a language server of its own, and that may be more useful to you if your editor supports multiple language servers per language.
+
+## Features
+
+This plugin adds the following features to `python-lsp-server`:
+
+- Type linting via Meta's Pyre (pyre-check)
+
 ## Installation
 
 To use this plugin, you need to install this plugin in the same virtualenv as `python-lsp-server` itself.
@@ -43,19 +51,33 @@ The configuration written by this plugin is:
     "exclude": [
         "/setup.py",
         ".*/build/.*"
-    ]
+    ],
+    "strict": true
 }
 ```
 
-The noteable difference from `pyre init` is the change to the search strategy (pep561 to all).
+The noteable difference from `pyre init` is the change to the search strategy ("pep561" to "all"), and turning on strict mode as the default. You may find strict mode a bit pedantic, but having worked with strict mode for several years, I highly recommend it.
 
-If the file is not present, the LSP error log, LSP output, and your editor's LSP messages will display an ABEND message containing the error from Pyre as it fails to run.
+If the `.pyre_configuration` file is not present (or has a syntax error), the LSP error log, LSP output, and your editor's LSP messages will display an ABEND message containing the error from Pyre as it fails to run.
 
-## Features
+## Squelching Pyre lint errors
 
-This plugin adds the following features to `python-lsp-server`:
+The recommended way of squelching a Pyre warning is to pick one of `# pyre-ignore` or `# pyre-fixme`. More precisely, suppose Pyre is indicating
 
-- Type linting via Meta's Pyre (pyre-check)
+```
+Missing global annotation [5]: Globally accessible variable `logger` has type `logging.Logger` but no type is specified.
+```
+
+at you, and you do not feel like typing your logger right now. On the line before, you can put either one of
+
+* `# pyre-ignore[5] Don't care about logger`
+* `# pyre-fixme[5] Resolve this when doing all logger work`
+
+to squelch the lint, and provide a hint to future you (or other readers of the code). This is a trivial example; it's easier to just type the logger.
+
+You do not need to match the number in the brackets, other than for ease of cross-reference with the [type errors documentation](https://pyre-check.org/docs/errors/).
+
+When you address the squelched error, Pyre will indicate that the comment is not suppressing a type error and can be removed.
 
 ## Developing
 
--- pylsp_pyre/plugin.py
+++ pylsp_pyre/plugin.py
@@ -1,3 +1,4 @@
+import dataclasses as dc
 import json
 import logging
 import subprocess
@@ -14,7 +15,19 @@ from pylsp import hookimpl
 
 logger: logging.Logger = logging.getLogger(__name__)
 # A logging prefix.
-PLUGIN: str = "[python-lsp-pyre]"
+PLUGIN = "[python-lsp-pyre]"
+
+
+@dc.dataclass
+class Settings:
+    enabled: bool = dc.field(init=False)
+    create_pyre_config: bool = dc.field(init=False)
+
+    def from_pylsp(self, config: pylsp_conf.Config) -> "Settings":
+        settings = config.plugin_settings("pyre")
+        self.enabled = getattr(settings, "enabled", True)
+        self.create_pyre_config = getattr(settings, "create-pyre-config", False)
+        return self
 
 
 @hookimpl
@@ -43,11 +56,13 @@ def pylsp_lint(
     Lints files (saved, not in-progress) and returns found problems.
     """
     logger.debug(f"Working with {document.path}, {is_saved=}")
-    maybe_create_pyre_config(config=config, workspace=workspace)
     if is_saved:
+        plugin_settings = Settings().from_pylsp(config=config)
+        maybe_create_pyre_config(settings=plugin_settings, workspace=workspace)
         with workspace.report_progress("lint: pyre check", "running"):
-            settings = config.plugin_settings("pyre")
-            diagnostics = run_pyre(workspace=workspace, document=document, settings=settings)
+            diagnostics = run_pyre(
+                workspace=workspace, document=document, settings=plugin_settings
+            )
         workspace.show_message(message=f"Pyre reported {len(diagnostics)} issue(s).")
         # Deal with location stuff by using unstructure() for now.
         return lsp_con.get_converter().unstructure(diagnostics)
@@ -79,14 +94,19 @@ def abend(message: str, workspace: pylsp_ws.Workspace) -> Dict[str, Any]:
 
 
 def run_pyre(
-    workspace: pylsp_ws.Workspace, document: pylsp_ws.Document, settings: Dict
+    workspace: pylsp_ws.Workspace, document: pylsp_ws.Document, settings: Settings
 ) -> List[Dict[str, Any]]:
     """
     Calls Pyre, converts output to internal structs
     """
     try:
-        data = really_run_pyre(root_path=workspace.root_path)
-        data = json.loads(data.decode("utf-8"))
+        pyre_out = really_run_pyre(root_path=workspace.root_path)
+        data = json.loads(pyre_out.decode("utf-8"))
+        # Pyre checks all files in the project, but at least with Kate, the LSP response is
+        # collected under the active file, making for misleading reading. Thus, filter on the
+        # path for now. This means that the Pyre output is less useful, because things like
+        # type changes on a commonly included attribute in one module will not show as
+        # problems in other modules unless the command line is used :(
         checks = [
             {
                 "source": "pyre",
@@ -99,6 +119,7 @@ def run_pyre(
                         line=(x["stop_line"] - 1), character=x["stop_column"]
                     ),
                 ),
+                "path": x["path"]
             }
             for x in data
             if document.path == f"{workspace.root_path}/{x['path']}"
@@ -136,9 +157,7 @@ def really_run_pyre(root_path: str) -> bytes:
         raise
 
 
-def maybe_create_pyre_config(
-    config: pylsp_conf.Config, workspace: pylsp_ws.Workspace
-) -> None:
+def maybe_create_pyre_config(settings: Settings, workspace: pylsp_ws.Workspace) -> None:
     """
     Initializes a .pyre_configuration file if `create-pyre-config` setting is enabled.
 
@@ -153,14 +172,15 @@ def maybe_create_pyre_config(
       ],
       "exclude": [
         "\/setup.py",
-        ".*\/build\/.*"
-      ]
+        ".*\/build\/.*",
+        ".*\/.pyre\/.*"
+      ],
+      "strict": true
     }
     """
     )
-    settings = config.plugin_settings("pyre")
     try:
-        if settings["create-pyre-config"]:
+        if settings.create_pyre_config:
             docroot = workspace.root_path
             path = Path(docroot).joinpath(".pyre_configuration")
             if not path.exists():
